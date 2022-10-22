a2c984a19ba5a7bffe9c65c2a379dd2ade7f9507
tdpearson <tylerdpearson@gmail.com>
2022-10-22 14:09:56 -0500

    use raw string to store regex pattern

--- data_store/urls.py
+++ data_store/urls.py
@@ -7,9 +7,9 @@ from api import config
 
 urlpatterns = [
     path('data/', MongoDataStore.as_view(), name='data-list'),
-    re_path('data/(?P<database>[^/]+)/$', MongoDataStore.as_view(), name='data-list'),
-    re_path('data/(?P<database>[^/]+)/(?P<collection>[^/]+)/$', DataStore.as_view(),name='data-detail'),
-    re_path('data/(?P<database>[^/]+)/(?P<collection>[^/]+)/(?P<id>[^/]+)/$', DataStoreDetail.as_view(),
+    re_path(r'data/(?P<database>[^/]+)/$', MongoDataStore.as_view(), name='data-list'),
+    re_path(r'data/(?P<database>[^/]+)/(?P<collection>[^/]+)/$', DataStore.as_view(),name='data-detail'),
+    re_path(r'data/(?P<database>[^/]+)/(?P<collection>[^/]+)/(?P<id>[^/]+)/$', DataStoreDetail.as_view(),
                            name='data-detail-id'),
 ]
 
