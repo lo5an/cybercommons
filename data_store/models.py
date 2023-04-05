01596a65e4d4e1ef37884fde307aab474da420a1
Rithsek99 <rithsek99@gmail.com>
2023-04-05 17:16:04 -0500

    modify public state of database or collection

--- data_store/models.py
+++ data_store/models.py
@@ -10,3 +10,22 @@ class dataStore(models.Model):
             ('datastore_create','Create DataStore Databases and Collections'),
         )
     pass
+
+class DatabasePermission(models.Model):
+    database_name = models.CharField(max_length=255)
+    isPublic = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.database_name
+class CollectionPermission(models.Model):
+    collection_name = models.CharField(max_length=255)
+    database = models.ForeignKey(DatabasePermission, on_delete=models.CASCADE, related_name='collections')
+    isPublic = models.BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.database.isPublic:
+            self.isPublic = False
+        super(CollectionPermission, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.collection_name} in {self.database.database_name}'
